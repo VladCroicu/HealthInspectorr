24d98d7ef3346d661c4323a9f11c3fbe3d9914ba
Zolianul <vladcroicu@yahoo.com>
2022-05-20 18:56:41 +0300

    Log-in function

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module healthinspectorproject.healthinspectorr {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens healthinspectorproject.healthinspectorr to javafx.fxml;
